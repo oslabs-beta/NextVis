565d81f8c12f97dc92981b89ddc9d28a9804621b
Jun Inton <juninton@gmail.com>
2024-12-04 19:02:40 -0800

    restructured project folder to focus on vs code extension, moved old files into D3 folder

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('nextFlow.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from nextFlow!');
+		vscode.window.showInformationMessage('Hello VS Code!');
 	});
 
 	context.subscriptions.push(disposable);
