106f707c9a7cc8c81404578e1157ca4fd555878b
SungGuk Jung <sunggukjung@sungguk-mbp.mynetworksettings.com>
2025-01-14 19:38:56 -0800

    Nextflow metric with cpu and memory usage update

--- dist/webview.js
+++ dist/webview.js
@@ -31642,21 +31642,21 @@ const createChart = (data) => {
           });
   
       // styles the node as a circle
-      // nodeEnter.append("circle")
-      //     .attr("r", 10)
-      //     .attr("width", 40)
-      //     .attr("height", 20)
-      //     .attr("fill", d => d._children ? "#555" : "#999")
-      //     .attr("stroke-width", 10);
+      nodeEnter.append("circle")
+          .attr("r", 10)
+          .attr("width", 40)
+          .attr("height", 20)
+          .attr("fill", d => d._children ? "#982933" : "#4B8F8C")
+          .attr("stroke-width", 10);
 
       // styles the node as a rectangle
-      nodeEnter.append("rect")
-        .attr("x", -20)
-        .attr("y", -10)
-        .attr("width", 40)
-        .attr("height", 20)
-        .attr("fill", d => d._children ? "red" : "blue")
-        .attr("stroke-width", 10);
+      // nodeEnter.append("rect")
+      //   .attr("x", -20)
+      //   .attr("y", -10)
+      //   .attr("width", 40)
+      //   .attr("height", 20)
+      //   .attr("fill", d => d._children ? "red" : "blue")
+      //   .attr("stroke-width", 10);
       
       // styles the text taken from data
       nodeEnter.append("text")
@@ -31728,22 +31728,6 @@ const createChart = (data) => {
   
     return svg.node();
 };
-// const flare = {
-//     name: "app",
-//     children: [
-//         {
-//             name: "/home",
-//             children: [{ name: "/about",
-//               children:[{ name: ":path*", children: [{name: ":/a"}, {name: ":/b"}, {name: ":/c"}] }]
-//               }, 
-//           { name: "/order", children: [{ name: '/order/:id', children: [{ name: ':item'}]}, { name: ':item' }]}]
-//           },
-//           { name: "/dashboard",
-//             children:[{ name: "/dashboard/user", children: [{name: "/dashboard/user/settings"}, {name: "/dashboard/user/config"}] }]
-//             }
-//         ],
-// };
-// const dendrogram = createChart(flare);
 
 const vscode = acquireVsCodeApi();
 
@@ -31762,6 +31746,7 @@ const loadButton = document.createElement("button");
 loadButton.type = "button";
 loadButton.id = "loadMiddleware";
 loadButton.textContent = "Load Middleware Tree";
+// loadButton.style.padding = '5px';
 loadButton.style.margin = '10px 0px 0px 0px'; // spacing
 loadButton.style.borderRadius = '10px'; // border radius
 
@@ -31813,7 +31798,7 @@ window.addEventListener("message", event => {
 
         chart.appendChild(dendrogram);
         title.textContent = `Middleware Tree for ${message.compName}`;
-        title.style.color = getRandomColor(); // line 240
+        // title.style.color = getRandomColor(); // line 240
         
       } else {
         vscode.postMessage({
@@ -31823,26 +31808,6 @@ window.addEventListener("message", event => {
       }
   }
 });
-
-// {
-//   "name": "mainMiddleware.ts",
-//   "children": [{
-//       "name": "middleware"
-//   }, {
-//       "name": "helloWorld"
-//   }, {
-//       "name": "authMiddleware",
-//       "children": [{
-//           "name": "/protected"
-//       }, {
-//           "name": "/login"
-//       }]
-//   }, {
-//       "name": "localeMiddleware"
-//   }, {
-//       "name": "customHeadersMiddleware"
-//   }]
-// }
 })();
 
 /******/ })()
