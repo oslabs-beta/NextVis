096c432b13a89b369812b7bd573d674611c228c7
SungGuk Jung <sunggukjung@sungguk-mbp.mynetworksettings.com>
2025-01-30 18:15:31 -0800

    extension.test.ts added

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,107 @@
-import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+// import * as assert from 'assert';
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import { activate, deactivate } from '../extension';
+import * as path from 'path';
+// import * as sinon from 'sinon';
+import * as extension from '../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+let registeredCommands: { [key: string]: Function } = {};
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+jest.mock('vscode', () => ({
+	ExtensionContext: jest.fn(),
+	window: {		
+	showInformationMessage: jest.fn(),
+	  createWebviewPanel: jest.fn().mockReturnValue({
+		webview: {
+		  html: '',
+          asWebviewUri: jest.fn().mockReturnValue('mockUri'),
+		  onDidReceiveMessage: jest.fn(),
+		  postMessage: jest.fn()
+		}
+	  }),
+	  showOpenDialog: jest.fn(),
+	  showErrorMessage: jest.fn()
+	},
+	commands: {
+	  registerCommand: jest.fn((command, callback) => {
+		// Store the callback so we can call it in tests
+		registeredCommands[command] = callback;
+		return { dispose: jest.fn() };
+	  }),
+	  executeCommand: jest.fn()
+	},
+	ViewColumn: {
+	  One: 1,
+	  Two: 2
+	},
+	Uri: {
+		file: jest.fn((filePath) => ({ fsPath: filePath }))
+	},
+	  showOpenDialog: jest.fn()
+  }),{ virtual: true });
+
+
+
+describe ('NextFlow Extension Test', () => {
+	let context : vscode.ExtensionContext;
+	let asWebviewUriMock: jest.Mock;
+
+	beforeEach (()=> {
+		jest.clearAllMocks();
+		registeredCommands = {};
+
+		asWebviewUriMock = jest.fn().mockReturnValue('mockUri');
+
+		(vscode.window.createWebviewPanel as jest.Mock).mockReturnValue({
+			webview: {
+				html: '',
+				asWebviewUri: asWebviewUriMock, // Mocking asWebviewUri function
+				onDidReceiveMessage: jest.fn(),
+				postMessage: jest.fn(),
+			},
+		});
+
+		context = {
+			subscriptions: [],
+			extensionPath: '/fake/path',
+			asAbsolutePath: jest.fn(p => p),
+		}as unknown as vscode.ExtensionContext;
+		});
+
+		describe('activate', () => {
+			test('should register the nextFlow.start command',  () => {
+			  activate(context);
+			//   console.log(context);
+			  // Check that the 'nextFlow.start' command is registered
+			  expect(vscode.commands.registerCommand).toHaveBeenCalledWith(
+				'nextFlow.start',
+				expect.any(Function)
+			  );
+			});
+
+			test('should create nextFlow and metrics webview panels when command is executed', ()=> {
+				activate(context);
+
+				const commandCallback = registeredCommands['nextFlow.start'];
+				expect(commandCallback).toBeDefined();
+
+				if (commandCallback) {
+					commandCallback(); // Trigger the callback
+				  }
+				
+				
+				expect(vscode.window.createWebviewPanel).toHaveBeenCalledTimes(1);
+
+				expect(vscode.window.createWebviewPanel).toHaveBeenCalledWith(
+					'nextFlow',
+					'NextFlow',
+					vscode.ViewColumn.One,
+					expect.objectContaining({enableScripts: true, retainContextWhenHidden: true})
+				);
+			});
+			// test('should call correct file path', () => {
+			// 	expect(asWebviewUriMock.mock.calls.length).toBe(1)
+			// });
+				
 	});
 });
